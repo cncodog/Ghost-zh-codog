708e5c60a9c4af9467ec437685048fc90e768b15
mac <tanbo_800@126.com>
2015-10-31 01:11:06 +0800

    commit -m 'topic add 注释'

--- core/server/third/third_topic.js
+++ core/server/third/third_topic.js
@@ -4,7 +4,8 @@
 
 var Models          = require('../models');
 var rp = require('request-promise');
-
+var qs = require('querystring');
+var  moment = require("moment");
 var ADD_TOPIC_URL = 'http://dockerclub.net/api/v1/topics';
 var DEL_TOPIC_URL ='http://dockerclub.net/api/v1/topic/de_collect';
 
@@ -15,15 +16,37 @@ function ThirdTopics () {
 ThirdTopics.prototype.addTopic = function (post,uid) {
 
   return Models.User.findOne({id: uid}).then(function (result) {
+      console.log('result' + JSON.stringify(result));
+      console.log('result' + JSON.stringify(post));
+        //用户信息
+        var actor = result.get('name');//作者
+        var email = result.get('email');//邮箱
+        var uuid  = result.get('uuid');
+        //post信息
+        var title = post.get('title');
+        var markdown = post.get('markdown');
+        var slug = post.get('slug');
+        var createtime =moment(post.get('created_at')).format("YYYY/MM/DD HH:mm:ss");
+        //var pl_time =moment(post.get('created_at'), "MM/DD/YYYY");
+        var pl_time  = moment(post.get('created_at')).format("YYYY/MM/DD");
+        var link_url = pl_time  +  '/' + slug;
+        //var now = moment();
+
+
+        var head_html = '** 作者: '+ actor + '发布了[《' +title+'》](http://blog.dockerclub.net/'+link_url+') '
+                        +'发布时间:'+ createtime + "**</p>" + markdown;
+
+
+      //  var head_markdown = '**来自xxx的博客，访问原始链接请查看** [内容](链接)';
         //定义数据
         var options = {
           method: 'POST',
           uri: ADD_TOPIC_URL,
           form: {
-            accesstoken:result.get('uuid'),
-            title: post.get('title'),
+            accesstoken:uuid,
+            title: title,
             tab: 'seminar',
-            content:post.get('markdown')
+            content:head_html
 
           },
           headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
@@ -34,8 +57,9 @@ ThirdTopics.prototype.addTopic = function (post,uid) {
               return eval('(' + body + ')');
           })
           .catch(function (err) {
+              console.log('同步失败' + err);
               return eval('{success:false}');
-            console.log('同步失败' + err);
+
         });
   }, function () {
       return Promise.reject(new errors.NotFoundError('无法找到当前用户'));
